17b56af0cc2eddf69dda1d4ffefa44aa330a4124
Dominique Makowski <dom.mak19@gmail.com>
2023-12-01 10:50:52 +0000

    update

--- analysis_verbal/make_word.py
+++ analysis_verbal/make_word.py
@@ -1,13 +1,14 @@
 import docx
 import pandas as pd
 
-df = pd.read_excel("raw_stimuli.xlsx")
+df = pd.read_excel("raw_stimuli.xlsx", sheet_name=1)
 
 document = docx.Document()
 for index, row in df.iterrows():
-    print(index)
+    # print(index)
     txt = df.loc[index, "Grammar-Corrected Response"]
     if pd.isnull(txt):
+        print(index)
         continue
     document.add_heading(str(index), 0)
     document.add_paragraph(txt)

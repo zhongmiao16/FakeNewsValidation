ba7ba9a58db9b610d4d27525b2bd5dae772af258
Dominique Makowski <dom.mak19@gmail.com>
2024-01-10 12:23:09 +0000

    update consent

--- Experiment/demographics.js
+++ Experiment/demographics.js
@@ -6,28 +6,17 @@ var demographics_browser_info = {
         date: new Date().toLocaleDateString("fr-FR"),
         time: new Date().toLocaleTimeString("fr-FR"),
     },
-    on_finish: function () {
-        data = jsPsych.data.get().filter({ screen: "browser_info" }).values()[0]
-        jsPsych.data.addProperties({
-            ["screen_height"]: data["height"],
-            ["screen_width"]: data["width"],
-        })
-        for (var key in data) {
-            if (
-                [
-                    "vsync_rate",
-                    "os",
-                    "mobile",
-                    "browser",
-                    "browser_version",
-                ].includes(key)
-            ) {
-                jsPsych.data.addProperties({
-                    [key]: data[key],
-                })
-            }
-        }
-        jsPsych.data.addProperties()
+    on_finish: function (data) {
+        dat = jsPsych.data.get().filter({ screen: "browser_info" }).values()[0]
+
+        // Rename
+        data["screen_height"] = dat["height"]
+        data["screen_width"] = dat["width"]
+
+        // Add URL variables - ?sona_id=x&exp=1
+        let urlvars = jsPsych.data.urlVariables()
+        data["sona_id"] = urlvars["sona_id"]
+        data["researcher"] = urlvars["exp"]
     },
 }
 
@@ -55,44 +44,37 @@ var demographics_participant_id = {
 }
 
 // Consent form ========================================================================
-function demographics_consent(experimenter = "DEFAULT") {
-    return {
-        type: jsPsychHtmlButtonResponse,
-        css_classes: ["narrow-text"],
-        stimulus:
-            // Logo and title
-            "<img src='https://seeklogo.com/images/N/nanyang-technological-university-logo-99C7E42213-seeklogo.com.png' width='150px' align='right'/><br><br><br><br><br>" +
-            "<h1>Informed Consent</h1>" +
-            // Overview
-            "<p align='left'><b>Invitation to Take Part</b><br>" +
-            "You are being invited to take part in a research study to further our understanding of Human psychology. Thank you for carefully reading this information sheet. This study is being conducted by Prof Annabel Chen from the School of Social Sciences, Nanyang Technological University, who is happy to be contacted if you have any questions.</p>" +
-            // Description
-            "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
-            "We are surveying adults to understand how people judge real and fake news articles. The whole experiment will take you <b>about 30S min</b> to complete. Please make you sure that you are in a quiet environment, and that you have time to complete it in one go.</p>" +
-            // Results and personal information
-            "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
-            "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. Please read this information carefully and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
-            "<p align='left'><b>Consent</b><br></p>" +
-            // Bullet points
-            "<li align='left'>I understand that by signing below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet</li>" +
-            "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalised in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
-            "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed and submitted the test/questionnaire.</li>" +
-            "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
-            "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publically available through secured scientific online data repositories.</li>" +
-            "</p>" +
-            // "<p align='left'>Your participation in this research will be kept completely confidential. Your responses are entirely anonymous, and no IP address or any identifiers is collected.</p>" +
-            // "<p align='left'><b>By participating, you agree to follow the instructions and provide honest answers.</b> If you do not wish to participate this survey, simply close your browser.</p>" +
-            // "<p>Please note that various checks will be performed to ensure the validity of the data.<br>We reserve the right to return your participation or prorate reimbursement should we detect non-valid responses (e.g., random pattern of answers, instructions not read, ...).</p>"
-            "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Prof Annabel Chen (annabelchen@ntu.edu.sg). This research has been approved (XX/XXXX/XX) by the ethics board of the university.</sup></sub></p>",
+var demographics_consent = {
+    type: jsPsychHtmlButtonResponse,
+    css_classes: ["narrow-text"],
+    stimulus:
+        // Logo and title
+        "<img src='https://seeklogo.com/images/N/nanyang-technological-university-logo-99C7E42213-seeklogo.com.png' width='150px' align='right'/><br><br><br><br><br>" +
+        "<h1>Informed Consent</h1>" +
+        // Overview
+        "<p align='left'><b>Invitation to Take Part</b><br>" +
+        "You are being invited to take part in a research study to further our understanding of Human psychology. Thank you for carefully reading this information sheet. This study is being conducted by Prof Annabel Chen from the School of Social Sciences, Nanyang Technological University, who is happy to be contacted if you have any questions.</p>" +
+        // Description
+        "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
+        "We are surveying adults to understand how people judge real and fake news articles. The whole experiment will take you <b>about 30S min</b> to complete. Please make you sure that you are in a quiet environment, and that you have time to complete it in one go.</p>" +
+        // Results and personal information
+        "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
+        "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. Please read this information carefully and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
+        "<p align='left'><b>Consent</b><br></p>" +
+        // Bullet points
+        "<li align='left'>I understand that by signing below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet</li>" +
+        "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalised in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
+        "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed and submitted the test/questionnaire.</li>" +
+        "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
+        "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publically available through secured scientific online data repositories.</li>" +
+        // Incentive
+        "<li align='left'>Please note that various checks will be performed to ensure the validity of the data. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>" +
+        "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate, simply close your browser.</li>" +
+        "</p>" +
+        "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Prof Annabel Chen (annabelchen@ntu.edu.sg). This research has been approved (XX/XXXX/XX) by the ethics board of the university.</sup></sub></p>",
 
-        choices: ["I read, understood, and I consent"],
-        data: { screen: "consent" },
-        on_finish: function () {
-            jsPsych.data.addProperties({
-                experimenter: experimenter,
-            })
-        },
-    }
+    choices: ["I read, understood, and I consent"],
+    data: { screen: "consent" },
 }
 
 // Thank you ========================================================================
@@ -177,7 +159,8 @@ var demographics_freetext = {
     questions: [
         {
             prompt: "What is your area of study?",
-            placeholder: "e.g., 'Business', 'Computer Science', 'Arts and Humanities'",
+            placeholder:
+                "e.g., 'Business', 'Computer Science', 'Arts and Humanities'",
             name: "discipline",
         },
         {
